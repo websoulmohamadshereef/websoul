0ee070e45ccb4d34f6e69546fdc8798d75c8ef82
websoulmohamadshereef <websoulmohamadshereef@gmail.com>
2020-05-31 14:05:46 +0530

    committing a test file

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BodyComponent } from './components/body/body.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BodyComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
